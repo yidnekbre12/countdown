9b12cd98e2eb3b96c72c1992aff670f05ed11b83
yidnekachew birhanu <119066592+yidnekbre12@users.noreply.github.com>
2023-03-24 07:57:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "18 Mar 2024";
+const newYears = "1 Jan 2024";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
